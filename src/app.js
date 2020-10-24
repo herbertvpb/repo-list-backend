e4d4be56a26bc4b4860eb142bb145a36fa2699a7
herbert <herbert.vpb@gmail.com>
2020-10-24 12:15:06 -0300

    challenge finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +9,117 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequest(request, response, next) {
+	const { method, url } = request;
+	const logLabel = `[${method.toUpperCase()}] ${url}`;
+	console.time(logLabel);
+	next();
+	console.timeEnd(logLabel);
+};
+
+function validateRepositoryId(request, response, next) {
+	const { id } = request.params;
+
+  if (!isUuid(id)) 
+    return response.status(400).json({ 
+      error: 'Invalid repository ID.' 
+    });
+  
+	return next();
+};
+
+function validateCreateRepositoryPayload(request, response, next) {
+  const body = request.body;
+
+  if (!body.hasOwnProperty('title')) 
+    return response.status(400).json({ error: 'Title is required.' });
+  if (!body.hasOwnProperty('url')) 
+    return response.status(400).json({ error: 'Url is required.' });
+  if (!body.hasOwnProperty('techs')) 
+    return response.status(400).json({ error: 'Techs is required.' });
+
+  return next();
+};
+
+app.use(logRequest);
+app.use('/repositories/:id', validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", validateCreateRepositoryPayload, (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({
+      error: 'Repository not found'
+    });
+  
+  const repository = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({
+      error: 'Repository not found'
+    });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({
+      error: 'Repository not found'
+    });
+  
+  const likes = repositories[repositoryIndex].likes + 1;
+
+  const repository = {
+    ...repositories[repositoryIndex],
+    likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+	console.log('|=========================================|');
+	console.log('|                                         |');
+	console.log(`| 👉️ Backend started at localhost:3333 👈️ |`);
+	console.log('|                                         |');
+	console.log('|=========================================|');
+});
